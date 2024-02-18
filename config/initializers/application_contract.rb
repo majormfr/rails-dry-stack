551dc4d3f4a72e355ff72a6430a15f9601fca76e
“Mohammad <fahadrao1718@gmail.com>
2024-02-18 06:16:15 +0500

    Boiler plate dry rb

---
+++ config/initializers/application_contract.rb
@@ -0,0 +1,8 @@
+class ApplicationContract < Dry::Validation::Contract
+  config.messages.default_locale = :en
+  config.messages.backend = :i18n
+
+  def self.call(*args, &block)
+    new.call(*args, &block)
+  end
+end
